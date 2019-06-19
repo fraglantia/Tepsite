b8c42b9fa0534cd92e8c6c7463b96ed80372ce8c
Steve <stevebezalel215@gmail.com>
2019-06-19 22:11:18 +0700

    Updatess

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Body extends React.Component {
+  render() {
+      return(
+          <div>
+              <h1>didibodi dibodibodi</h1>
+          </div>
+      )
+  }
+}
+
+export default Body;
