ecdb30a557342e40282cae375706c244853b8914
Steve <stevebezalel215@gmail.com>
2019-06-20 14:51:53 +0700

    added social media icons and some animations

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faFacebook, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 class Footer extends React.Component {
   render() {
       return(
           <div className='footer'>
-              <p>sbig, 2019</p>
+                <div className='icons'>
+                    <div className='icon'>
+                        <a href='#'>
+                            <FontAwesomeIcon icon={faInstagram} size='2x'/>
+                        </a>
+                    </div>
+                    <p>•</p>
+                    <div className='icon'>
+                        <a href='#'>
+                            <FontAwesomeIcon icon={faFacebook} size='2x'/>
+                        </a>
+                    </div>
+                    <p>•</p>
+                    <div className='icon'>
+                        <a href='#'>
+                            <FontAwesomeIcon icon={faTwitter} size='2x'/>
+                        </a>
+                    </div>
+                    <p>•</p>
+                    <div className='icon'>
+                        <a href='#'>
+                            <FontAwesomeIcon icon={faGithub} size='2x'/>
+                        </a>
+                    </div>
+                </div>
+                <div className='text'>
+                    <p>sbig, 2019</p>
+                </div>
           </div>
+          
       )
   }
 }
